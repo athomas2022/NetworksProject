286c008a4137b473d008d0c79da9ee133f0fcf22
alext <alex.thomas@snet.net>
2025-10-20 21:55:42 -0400

    Working Receive Function Function that successfully receives and prints a message sent from another client

--- client.py
+++ client.py
@@ -1,13 +1,15 @@
+import scapy
 from scapy.layers.inet import IP, Ether, TCP
 from scapy.packet import Raw
 from scapy.config import conf
 from scapy.sendrecv import sendp, send, sniff
 import socket
+from time import sleep
 
 
 source_name = socket.gethostname()
 source_addr = socket.gethostbyname(source_name)
-test_dest = source_addr
+test_dest = '163.118.57.184'
 
 
 def message_send(message_data):
@@ -23,14 +25,24 @@ def make_ip_layer(srca, dsta):
 
 def message_receive():
     def recv_protocol(packet):
-        if test_dest in packet:
-            if Raw in packet:
-                return packet[Raw].load
-            else:
-                return 'packet found but no payload'
+        if Raw in packet:
+            packet.summary()
+            print(packet[Raw].load)#.decode('utf-8', errors='replace'))
+        else:
+            print('packet found but no payload')
 
-    print(sniff(prn=recv_protocol, store=0))
+    capture = sniff(prn=recv_protocol, filter='src 163.118.57.184 and dst 163.118.57.142', count=1)
+    capture.summary()
 
 
-msg_data = "Hello World!"
-message_send(msg_data)
+def sock_recv():
+    print('running')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('163.118.57.142', 12345))
+    print('listening...')
+    s.listen(1)
+    print('accepting...')
+    conn, addr = s.accept()
+    print(f"Connected by {addr}")
+    d = conn.recv(1024)
+    print(d.decode('utf-8'))
