06917bfc38fd83da6338116bcdf071efa00c81c5
Tlex-Ahomas <alex.thomas@snet.net>
2025-10-20 21:48:59 -0400

    Working Send Function Creation of a send function that successfully sends data to another client

--- client.py
+++ client.py
@@ -1,20 +1,41 @@
-from scapy.layers.inet import IP, Ether, TCP
+from scapy.layers.inet import IP, Ether, TCP, UDP
 from scapy.packet import Raw
 from scapy.config import conf
 from scapy.sendrecv import sendp, send, sniff
 import socket
+import threading
+import json
 
 
-source_name = socket.gethostname()
-source_addr = socket.gethostbyname(source_name)
-test_dest = source_addr
+# source_name = socket.gethostname()
+# source_addr = socket.gethostbyname(source_name)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+source_addr = s.getsockname()[0]
+s.close()
+print(source_addr)
+test_dest = '163.118.57.142'
 
 
 def message_send(message_data):
+    eth = Ether(dst="ff:ff:ff:ff:ff:ff")
     ip_layer = make_ip_layer(source_addr, test_dest)
     payload = Raw(load=message_data)
-    complete_packet = ip_layer / payload
-    send(complete_packet)
+    complete_packet = eth / ip_layer / TCP(sport=1234, dport=4445, flags="PA") / payload
+    complete_packet.show()
+    sendp(complete_packet)
+
+
+def send_2(message_data):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('connecting...')
+    sock.connect((test_dest, 12345))
+    payload = {"message": message_data, "value": 123}
+    payload_data = json.dumps(payload).encode('utf-8')
+    sock.sendall(payload_data)
+    sock.close()
+
+
 
 
 def make_ip_layer(srca, dsta):
@@ -23,14 +44,20 @@ def make_ip_layer(srca, dsta):
 
 def message_receive():
     def recv_protocol(packet):
-        if test_dest in packet:
-            if Raw in packet:
-                return packet[Raw].load
-            else:
-                return 'packet found but no payload'
+        if Raw in packet:
+             print(packet[Raw].load)
+        else:
+            print('packet found but no payload')
 
-    print(sniff(prn=recv_protocol, store=0))
+    print(sniff(prn=recv_protocol, filter=f'src {source_addr} and dst {test_dest}', count=1))
 
 
 msg_data = "Hello World!"
-message_send(msg_data)
+
+#sniff_thread = threading.Thread(target=message_receive)
+#send_thread = threading.Thread(target=message_send, args=(msg_data,))
+
+#sniff_thread.start()
+#send_thread.start()
+
+send_2(msg_data)
